cd89e86cd82a7fc771d5b2d7ee2989cf8073b81d
Hai Feng Kao <khaifeng@gmail.com>
2024-11-02 18:35:25 +0800

    Initial Commit

---
+++ DCI_and_JRPG/DCI_and_JRPGApp.swift
@@ -0,0 +1,17 @@
+//
+//  DCI_and_JRPGApp.swift
+//  DCI_and_JRPG
+//
+//  Created by Lono on 2024/11/2.
+//
+
+import SwiftUI
+
+@main
+struct DCI_and_JRPGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
